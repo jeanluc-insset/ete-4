5a767778f3b6ec6824c9246f4e1ea2c330330381
Jean-Luc DELEAGE <jldeleage@free.fr>
2018-11-01 10:43:42 +0100

    Create the class ExpressionBuilder
    Currently, the class contains nothing but an empty navTo method.

---
+++ ete/gel/gel-test-utils/src/main/java/fr/upjv/mis/ete/gel/test/utils/ExpressionBuilder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.upjv.mis.ete.gel.test.utils;
+
+import fr.upjv.mis.ete.gel.GelExpression;
+import fr.upjv.mis.ete.mof.MofFeature;
+
+
+/**
+ *
+ * @author jldeleage
+ */
+public class ExpressionBuilder {
+
+    public GelExpression    navTo(GelExpression inFrom, MofFeature inTo) {
+        return null;
+    }
+
+
+}
