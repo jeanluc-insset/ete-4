1db80636d045c8bc1bfa93768ff497edf24b46ab
Jean-Luc DELEAGE <jldeleage@free.fr>
2018-11-02 09:22:54 +0100

    Make public the checkEquals method in the ExpressionBuilder (gel-test-utils project)

--- ete/gel/gel-impl/src/test/java/fr/upjv/mis/ete/gel/TreeBuilderTest.java
+++ ete/gel/gel-impl/src/test/java/fr/upjv/mis/ete/gel/TreeBuilderTest.java
@@ -18,23 +18,23 @@ import static org.junit.Assert.*;
  * @author jldeleage
  */
 public class TreeBuilderTest {
-    
+
     public TreeBuilderTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         TreeBuilder.initFactories();
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -55,33 +55,12 @@ public class TreeBuilderTest {
     /**
      * Test tree built by a TreeBuilder for inExpression.
      */
-    public void testBuild(String inExpression, GelExpression expResult) {
+    public void testBuild(String inExpression, GelExpression expResult, ExpressionBuilder expBuilder) throws Exception {
         System.out.println("build");
         Object inContext = null;
         TreeBuilder instance = new TreeBuilder();
         GelExpression result = instance.build(inExpression, inContext);
-        checkEquals(expResult, result);
-    }
-
-
-    protected void checkEquals(GelExpression expResult, GelExpression result) {
-        assertEquals(expResult.getClass(), result.getClass());
-        List<GelExpression>     expOperands = expResult.getOperand();
-        List<GelExpression>     resultOperands = result.getOperand();
-        if (expOperands == null) {
-            if (resultOperands == null) {
-                return;
-            }
-            fail("No operand collection expected");
-        }
-        Iterator<GelExpression> expIterator = expOperands.iterator();
-        Iterator<GelExpression> resultIterator = expOperands.iterator();
-        while (expIterator.hasNext() && resultIterator.hasNext()) {
-            GelExpression expOp = expIterator.next();
-            GelExpression resultOp = resultIterator.next();
-            checkEquals(expOp, resultOp);
-        }
-        assertFalse(expIterator.hasNext() || resultIterator.hasNext());
+        expBuilder.checkEquals(expResult, result);
     }
 
 
